b0e8eef263036fe00bf7e4b19acf1c07a3ef43b1
anacsmartins <anamartins.sistemas@gmail.com>
2024-12-03 11:38:26 -0500

    gix: route

--- src/infrastructure/config/routers.ts
+++ src/infrastructure/config/routers.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import { Controller } from "../../application/controller";
 
 const router = express.Router();
+const controller = new Controller(); // Instanciando fora para reutilização
 
-// Definindo as rotas
-router.post(
-  "/name",
-  //(req: any, res: any) => (new Controller).simulate(req, res)
+// Definindo a rota
+router.get(
+  "/home",
+  (req, res) => controller.simulate(req, res)
 );
 
 export default router;
